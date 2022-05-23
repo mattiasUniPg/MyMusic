9df0ebaf785f20c089081b047c3c6d37bb39b50e
Matthias C <Zibetto1997@gmail.com>
2022-05-23 16:12:36 +0200

    fix

--- MyMusic/Controllers/HomeController.cs
+++ MyMusic/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace MyMusic.Controllers
         {
             var res = dBManager.GetAllBrani().Where(x => x.IDBrano == brano.IDBrano).FirstOrDefault();
             if (res != null)
-                dBManager.EditBrano(brano);
+                dBManager.ModificaBrano(brano);
 
             return RedirectToAction("Index");
         }
@@ -49,6 +49,96 @@ namespace MyMusic.Controllers
         public IActionResult Aggiungi(BraniViewModel brano)
         {
             dBManager.AggiungiBrano(brano);
+            return RedirectToAction("Index");
+        }
+        //Album
+        [HttpGet]
+        public IActionResult ModificaAlbum(int id)
+        {
+            var album = dBManager.GetAllAlbum().Where(x => x.IDAlbum == id).FirstOrDefault();
+            return View(album);
+        }
+
+        [HttpPost]
+        public IActionResult ModificaAlbum(AlbumViewModel album)
+        {
+            var res = dBManager.GetAllAlbum().Where(x => x.IDAlbum == album.IDAlbum).FirstOrDefault();
+            if (res != null)
+                dBManager.ModificaAlbum(album);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AggiungiAlbum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AggiungiAlbum(AlbumViewModel album)
+        {
+            dBManager.AggiungiAlbum(album);
+            return RedirectToAction("Index");
+        }
+        //Band
+        [HttpGet]
+        public IActionResult ModificaBand(int id)
+        {
+            var band = dBManager.GetAllBand().Where(x => x.IDBand == id).FirstOrDefault();
+            return View(band);
+        }
+
+        [HttpPost]
+        public IActionResult ModificaBand(BandViewModel band)
+        {
+            var res = dBManager.GetAllBand().Where(x => x.IDBand == band.IDBand).FirstOrDefault();
+            if (res != null)
+                dBManager.ModificaBand(band);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AggiungiBand()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AggiungiBand(BandViewModel band)
+        {
+            dBManager.AggiungiBand(band);
+            return RedirectToAction("Index");
+        }
+        //Artista
+        [HttpGet]
+        public IActionResult ModificaArtista(int id)
+        {
+            var artista = dBManager.GetAllArtista().Where(x => x.IDArtista == id).FirstOrDefault();
+            return View(artista);
+        }
+
+        [HttpPost]
+        public IActionResult ModificaArtista(ArtistaViewModel artista)
+        {
+            var res = dBManager.GetAllArtista().Where(x => x.IDArtista == artista.IDArtista).FirstOrDefault();
+            if (res != null)
+                dBManager.ModificaArtista(artista);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AggiungiArtista()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AggiungiArtista(ArtistaViewModel artista)
+        {
+            dBManager.AggiungiArtista(artista);
             return RedirectToAction("Index");
         }
 
--- MyMusic/Repository/DBManager.cs
+++ MyMusic/Repository/DBManager.cs
@@ -34,7 +34,7 @@ namespace MyMusic.Repository
             return braniList;
         }
 
-        public int EditBrano(BraniViewModel brano)
+        public int ModificaBrano(BraniViewModel brano)
         {
             string sql = @"UPDATE Brani
                        SET [Titolo] = @Titolo
@@ -81,6 +81,207 @@ namespace MyMusic.Repository
             return command.ExecuteNonQuery();
         }
 
+        public List<BandViewModel> GetAllBand()
+        {
+            List<BandViewModel> bandList = new List<BandViewModel>();
+            string sql = @"SELECT * 
+                        FROM Band";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var band = new BandViewModel
+                {
+                    IDBand = Convert.ToInt32(reader["IDBand"]),
+                    NomeBand = reader["NomeBand"].ToString(),
+                    Immagine = reader["Immagine"].ToString(),
+                    Genere = reader["Genere"].ToString(),
+                    AnnoUscitaAlbum = Convert.ToDateTime(reader["AnnoUscitaAlbum"])
+                };
+                bandList.Add(band);
+            }
+            return bandList;
+        }
+
+        public int ModificaBand(BandViewModel band)
+        {
+            string sql = @"UPDATE Band
+                       SET [NomeBand] = @NomeBand
+                          ,[Immagine] = @Immagine
+                          ,[Genere] = @Genere
+                          ,[AnnoUscitaAlbum] = @AnnoUscitaAlbum
+     
+                       WHERE IDBand =@IDBand";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@NomeBand", band.NomeBand);
+            command.Parameters.AddWithValue("@Immagine", band.Immagine);
+            command.Parameters.AddWithValue("@Genere", band.Genere);
+            command.Parameters.AddWithValue("@AnnoUscitaAlbum", band.AnnoUscitaAlbum);
+            command.Parameters.AddWithValue("@IDBand", band.IDBand);
+
+            return command.ExecuteNonQuery();
+        }
+        public int AggiungiBand(BandViewModel band)
+        {
+            string sql = @"INSERT INTO Band
+            ([NomeBand]
+           ,[Immagine]
+           ,[Genere]
+           ,[AnnoUscitaAlbum] )
+                       VALUES (@NomeBand,@Immagine,@Genere,@AnnoUscitaAlbum ) ";
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@NomeBand", band.NomeBand);
+            command.Parameters.AddWithValue("@Immagine", band.Immagine);
+            command.Parameters.AddWithValue("@Genere", band.Genere);
+            command.Parameters.AddWithValue("@AnnoUscitaAlbum", band.AnnoUscitaAlbum);
+
+            return command.ExecuteNonQuery();
+        }
+
+        public List<ArtistaViewModel> GetAllArtista()
+        {
+            List<ArtistaViewModel> artistaList = new List<ArtistaViewModel>();
+            string sql = @"SELECT * 
+                        FROM Artisti";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var artista = new ArtistaViewModel
+                {
+                    IDArtista = Convert.ToInt32(reader["IDArtista"]),
+                    Nome = reader["Nome"].ToString(),
+                    Cognome = reader["Cognome"].ToString(),
+                    Alias = reader["Alias"].ToString(),
+                    BandMusicale = reader["Alias"].ToString(),
+                    Tipo = reader["Alias"].ToString()
+
+                };
+                artistaList.Add(artista);
+            }
+            return artistaList;
+        }
+
+
+        public int ModificaArtista(ArtistaViewModel artista)
+        {
+            string sql = @"UPDATE Artisti
+                       SET [Nome] = @Nome
+                          ,[Cognome] = @Cognome
+                          ,[Alias] = @Alias
+                          ,[BandMusicale] = @BandMusicale
+                          ,[Tipo] = @Tipo
+                          
+                     WHERE IDArtista =@IDArtista";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@NomeBand", artista.Nome);
+            command.Parameters.AddWithValue("@Immagine", artista.Cognome);
+            command.Parameters.AddWithValue("@Genere", artista.Alias);
+            command.Parameters.AddWithValue("@AnnoUscitaAlbum", artista.BandMusicale);
+            command.Parameters.AddWithValue("@AnnoUscitaAlbum", artista.Tipo);
+
+            return command.ExecuteNonQuery();
+        }
+        public int AggiungiArtista(ArtistaViewModel artista)
+        {
+            string sql = @"INSERT INTO Artisti
+            ([Nome]
+           ,[Cognome]
+           ,[Alias]
+           ,[BandMusicale]
+           ,[Tipo] )
+                       VALUES (@Nome,@Cognome,@Alias,@BandMusicale,@Tipo ) ";
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Nome", artista.Nome);
+            command.Parameters.AddWithValue("@Cognome", artista.Cognome);
+            command.Parameters.AddWithValue("@Alias", artista.Alias);
+            command.Parameters.AddWithValue("@BandMusicale", artista.BandMusicale);
+            command.Parameters.AddWithValue("@Tipo", artista.Tipo);
+
+            return command.ExecuteNonQuery();
+        }
+
+        public List<AlbumViewModel> GetAllAlbum()
+        {
+            List<AlbumViewModel> albumList = new List<AlbumViewModel>();
+            string sql = @"SELECT * 
+                        FROM Album";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var album = new AlbumViewModel
+                {
+                    IDAlbum = Convert.ToInt32(reader["IDArtista"]),
+                    TitoloAlbum = reader["TitoloAlbum"].ToString(),
+                    BandAlbum = reader["BandAlbum"].ToString(),
+                    BraniAlbum = reader["BraniAlbum"].ToString(),
+                    AnnoUscitaAlbum = Convert.ToDateTime(reader["AnnoUscitaAlbum"])
+
+
+                };
+                albumList.Add(album);
+            }
+            return albumList;
+        }
+
+        public int ModificaAlbum(AlbumViewModel album)
+        {
+            string sql = @"UPDATE Album
+                       SET [TitoloAlbum] = @TitoloAlbum
+                          ,[BandAlbum] = @BandAlbum
+                          ,[BraniAlbum] = @BraniAlbum
+                          ,[AnnoUscitaAlbum] = @AnnoUscitaAlbum
+                          
+                     WHERE IDAlbum =@IDAlbum";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@TitoloAlbum", album.TitoloAlbum);
+            command.Parameters.AddWithValue("@BandAlbum", album.BandAlbum);
+            command.Parameters.AddWithValue("@BraniAlbum", album.BraniAlbum);
+            command.Parameters.AddWithValue("@AnnoUscitaAlbum", album.AnnoUscitaAlbum);
+
+            return command.ExecuteNonQuery();
+        }
+        public int AggiungiAlbum(AlbumViewModel album)
+        {
+            string sql = @"INSERT INTO Album
+            ([TitoloAlbum]
+           ,[BandAlbum]
+           ,[BraniAlbum]
+           ,[AnnoUscitaAlbum] )
+                       VALUES (@TitoloAlbum,@BandAlbum,@BraniAlbum,@AnnoUscitaAlbum ) ";
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@TitoloAlbum", album.TitoloAlbum);
+            command.Parameters.AddWithValue("@BandAlbum", album.BandAlbum);
+            command.Parameters.AddWithValue("@BraniAlbum", album.BraniAlbum);
+            command.Parameters.AddWithValue("@AnnoUscitaAlbum", album.AnnoUscitaAlbum);
+
+            return command.ExecuteNonQuery();
+        }
 
     }
 }
